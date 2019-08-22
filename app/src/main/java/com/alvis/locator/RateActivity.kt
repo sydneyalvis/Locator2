295db0ce84968129522f2657725e0adbd895e03c
sydneyalvis <sydsydney896@gmail.com>
2019-08-22 22:05:49 +0300

    Initial commit

---
+++ app/src/main/java/com/alvis/locator/RateActivity.kt
@@ -0,0 +1,12 @@
+package com.alvis.locator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RateActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rate)
+    }
+}
---
+++ app/src/main/java/com/alvis/locator/contractor_profile.kt
@@ -0,0 +1,12 @@
+package com.alvis.locator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class contractor_profile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contractor_profile)
+    }
+}
